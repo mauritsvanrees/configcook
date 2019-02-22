2f17fd1a9eae870e2cd0f8bb836af1b0ab745671
Maurits van Rees <maurits@vanrees.org>
2019-02-22 11:58:20 +0100

    Added TODOs in config.py

--- src/configcook/config.py
+++ src/configcook/config.py
@@ -5,4 +5,6 @@ from six.moves.configparser import ConfigParser
 def parse_config(path):
     parser = ConfigParser()
     parser.read([path])
+    # TODO: support 'extends = path1 path2'
+    # TODO: support urls
     return parser
