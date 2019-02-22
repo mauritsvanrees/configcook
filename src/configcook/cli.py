58aa61973dcac31541f9df9db3d76cb1640809e7
Maurits van Rees <maurits@vanrees.org>
2019-02-22 09:23:17 +0100

    start on cli

---
+++ src/configcook/cli.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def parse_options():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+        default=False,
+        help="Verbose mode")
+
+    options = parser.parse_args()
+    return options
+
+
+
+def main():
+    options = parse_options()
+    if options.verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+    logging.basicConfig(level=loglevel,
+                    format="%(levelname)s: %(message)s")
+    logger.debug('Only shown when --verbose is used.')
+    logger.info('Hello, I will be your config cook today.')
+    return
