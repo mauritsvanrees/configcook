d7c78771583692d8ce21859cfe55af32b91a81d2
Maurits van Rees <maurits@vanrees.org>
2019-02-23 00:58:52 +0100

    Load extensions with pip install if needed.

--- src/configcook/main.py
+++ src/configcook/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .config import parse_config
+from .utils import execute_command
 import logging
 import pkg_resources
 import sys
@@ -26,6 +27,7 @@ class ConfigCook(object):
             logger.error("Section 'configcook' missing from config file.")
             sys.exit(1)
         logger.debug('configcook in sections.')
+        # We could do self._pip('freeze') here as start to see what we have got.
         if 'extensions' in self.config.options('configcook'):
             self._extension_names = self.config.get(
                 'configcook', 'extensions'
@@ -37,31 +39,65 @@ class ConfigCook(object):
 
         logger.debug('End of ConfigCook call.')
 
+    def _pip(self, *args):
+        """Run a pip command in a shell."""
+        # TODO: read extra pip options from configcook or maybe recipe
+        # section.
+        cmd = ['pip']
+        cmd.extend(args)
+        return execute_command(cmd)
+
     def _load_extensions(self):
         logger.debug('Loading extensions.')
-        group = 'configcook.extension'
-        entrypoints = list(pkg_resources.iter_entry_points(group=group))
-        if not entrypoints:
-            logger.error('No entrypoints have been registered for %s.', group)
-            return
-
         for extension_name in self._extension_names:
-            extension = None
-            # Grab the function or class that is the actual plugin.
-            for entrypoint in entrypoints:
-                if entrypoint.name == extension_name:
-                    logger.debug(
-                        'Found entry point for extension %s.', extension_name
-                    )
-                    break
-            else:
-                logger.error(
-                    'Could not find entry point for extension %s.',
-                    extension_name,
+            self._load_extension(extension_name)
+        logger.debug('Loaded extensions.')
+
+    def _load_extension(self, extension_name):
+        logger.debug('Loading extension %s.', extension_name)
+        # This will either find an entrypoint or sys.exit(1).
+        entrypoint = self._find_entrypoint(extension_name)
+        # Instantiate the extension.
+        # TODO: we probably want to pass something, like self.config.
+        extension = entrypoint.load()
+        logger.info('Loaded extension %s.', extension_name)
+        self.extensions.append(extension)
+
+    def _find_entrypoint(self, extension_name, install=True):
+        """Find an entry point for this extension.
+
+        When install=True, we can try a pip install.
+        """
+        logger.debug(
+            'Searching entry point for extension  %s.', extension_name
+        )
+        group = 'configcook.extension'
+        for entrypoint in pkg_resources.iter_entry_points(group=group):
+            if entrypoint.name == extension_name:
+                logger.debug(
+                    'Found entry point for extension %s.', extension_name
                 )
-                sys.exit(1)
-            # Instantiate the extension.
-            # TODO: we probably want to pass something, like self.config.
-            extension = entrypoint.load()
-            logger.debug('Loaded extension %s.', extension_name)
-            self.extensions.append(extension)
+                return entrypoint
+        if not install:
+            logger.error(
+                'We cannot install an entry point for extension %s.',
+                extension_name,
+            )
+            sys.exit(1)
+        logger.debug(
+            'We do not yet have an entry point for extension %s.',
+            extension_name,
+        )
+        # We probably want to support package:name.
+        package_name = extension_name.split(':')[0]
+        logger.info('Trying to install package %s.', package_name)
+        # TODO: catch error in this command.
+        result = self._pip('install', package_name)
+        print(result)
+        # Retry, but this time do not allow to install.
+        logger.info(
+            'Retrying searching for entry point for extension %s after install of %s.',
+            extension_name,
+            package_name,
+        )
+        return self._find_entrypoint(extension_name, install=False)
