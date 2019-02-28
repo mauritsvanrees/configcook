f86431792b1542289e01b7a4fe347b4fc31b06ff
Maurits van Rees <maurits@vanrees.org>
2019-02-28 10:20:38 +0100

    Moved zc.buildout configparser.py to new _vendor/ dir.
    This makes it clearer that we should not change this file.
    Well, except for calling the 'black' code editor on it for good measure.

---
+++ src/configcook/_vendor/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+configcook._vendor is for vendoring dependencies of configcook
+to prevent needing configcook to depend on something external.
+
+Especially, we may copy over individual files from zc.buildout.
+
+Files inside of pip._vendor should be considered immutable and should only be
+updated to versions from upstream.
+"""
--- src/configcook/config.py
+++ src/configcook/config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .configparser import parse
+from ._vendor.configparser import parse
 import os
 
 
