721c35f0538cc44aa688083fc364869cb9df7ad6
Maurits van Rees <maurits@vanrees.org>
2019-03-06 09:52:56 +0100

    Start Python debugger on exception.
    Simplified from the version of zc.buildout.

--- src/configcook/cli.py
+++ src/configcook/cli.py
@@ -3,6 +3,7 @@ from .main import ConfigCook
 from argparse import ArgumentParser
 
 import logging
+import sys
 
 
 logger = logging.getLogger(__name__)
@@ -18,6 +19,14 @@ def parse_options():
         default=False,
         help="Verbose mode",
     )
+    parser.add_argument(
+        "-D",
+        "--debug",
+        action="store_true",
+        dest="debug",
+        default=False,
+        help="Start Python debugger when exception occurs",
+    )
 
     options = parser.parse_args()
     return options
@@ -32,5 +41,16 @@ def main():
     logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
     logger.debug("Only shown when --verbose is used.")
     logger.info("Hello, I will be your config cook today.")
-    cook = ConfigCook(options)
-    cook()
+    try:
+        cook = ConfigCook(options)
+        cook()
+    except Exception:
+        exc_info = sys.exc_info()
+        import pdb
+        import traceback
+
+        traceback.print_exception(*exc_info)
+        if options.debug:
+            sys.stderr.write("\nStarting pdb:\n")
+            pdb.post_mortem(exc_info[2])
+        sys.exit(1)
